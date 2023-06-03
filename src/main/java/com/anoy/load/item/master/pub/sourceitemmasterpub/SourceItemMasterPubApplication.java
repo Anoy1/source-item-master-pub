c20c5cb5b9f75ba0261602c45cff7084b511a1f4
amianoybiswas <amianoybiswas@gmail.com>
2023-06-04 01:54:29 +0530

    ANOY | PUBLISHER CHANGES

--- src/main/java/com/anoy/load/item/master/pub/sourceitemmasterpub/SourceItemMasterPubApplication.java
+++ src/main/java/com/anoy/load/item/master/pub/sourceitemmasterpub/SourceItemMasterPubApplication.java
@@ -2,12 +2,18 @@ package com.anoy.load.item.master.pub.sourceitemmasterpub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SourceItemMasterPubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SourceItemMasterPubApplication.class, args);
+		new SpringApplicationBuilder(SourceItemMasterPubApplication.class)
+		.properties("spring.config.name:bootstrap")
+		.build()
+		.run(args);
+		//SpringApplication.run(SourceItemMasterPubApplication.class, args);
 	}
 
 }
