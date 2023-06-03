9ab53321360f9891e998b443c72bdd4e20e5e82f
amianoybiswas <amianoybiswas@gmail.com>
2023-06-03 17:58:41 +0530

    ANOY | Publisher creation

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.12"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.anoy.load.item.master.pub"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.oracle.database.jdbc:ojdbc8")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/anoy/load/item/master/pub/sourceitemmasterpub/SourceItemMasterPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoy.load.item.master.pub.sourceitemmasterpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceItemMasterPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
